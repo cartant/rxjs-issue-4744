89dbd66039b978fdf33a786b3f7ca16447a2d1dc
Nicholas Jamieson <nicholas@cartant.com>
2020-02-09 11:07:14 +1000

    chore: Add harness.

---
+++ index.js
@@ -0,0 +1,75 @@
+const http = require("http");
+
+const content = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>fetch harness</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <script>
+      console.log("fetching data...");
+      fetch("/data")
+        .then(response => {
+          console.log("data:", response.headers);
+          return response.text();
+        })
+        .then(text => {
+          console.log("data:", text);
+        });
+      console.log("fetching data-chunked...");
+      fetch("/data-chunked")
+        .then(response => {
+          console.log("data-chunked:", response.headers);
+          return response.text();
+        })
+        .then(text => {
+          console.log("data-chunked:", text);
+        });
+    </script>
+  </body>
+</html>
+`;
+
+const data = "[0123456789]";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  if (req.url === "/") {
+    res.end(content.trim());
+    return;
+  }
+
+  if (req.url === "/data") {
+    res.end(data);
+    return;
+  }
+
+  if (req.url === "/data-chunked") {
+    // https://github.com/nodejs/node/issues/30182#issuecomment-548491899
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+      "Transfer-Encoding": "chunked",
+      "Content-Length": data.length * 3
+    });
+    res.flushHeaders();
+    setTimeout(() => {
+      res.write(data);
+    }, 1000);
+    setTimeout(() => {
+      res.write(data);
+    }, 2000);
+    setTimeout(() => {
+      res.write(data);
+      res.end();
+    }, 3000);
+    return;
+  }
+
+  res.writeHead(404);
+  res.end();
+});
+
+server.listen(8008);
